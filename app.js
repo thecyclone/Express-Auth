ca1bee7cbc828716f7528915aac4b21a6aa3c317
Akash Deep Singh <akash14131@iiitd.ac.in>
2016-05-24 05:47:36 +0530

    fixed error messages

--- app.js
+++ app.js
@@ -59,23 +59,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next){
     res.status(err.status || 500);
-    res.render('error.html', {
-      message: err.message,
-      error: err
+    res.send({
+        message: err.message,
+        error: err
     });
+   return;
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next){
   res.status(err.status || 500);
-  res.render('error.html', {
-    message: err.message,
-    error: {}
+  res.send({
+      message: err.message,
+      error: {}
   });
+  return;
 });
 
 
